58dac9f9f0e20963f45735a72f2c59e9b0da73c4
Julio Cesar <juliocesarfi@comunidad.unam.mx>
2020-06-11 23:13:40 -0500

    Adding funtionality to form and save in a variable whit module request

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 app=Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route('/procesar', methods=['POST'])
+def procesar():
+    descripcion = request.form.get("descripcion")
+    return render_template("resultado.html", descripcion=descripcion)
+
 if __name__=="__main__":
     app.run(debug=True)
